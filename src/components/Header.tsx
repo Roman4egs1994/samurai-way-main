e24f9c97952fc2c9a644bd6fd8df897ab560aa6c
Roman <R.rybkin94@gmail.com>
2022-11-10 14:10:09 +0400

    Added component Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../img/logo.png";
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img className="logotype" src={logo} alt="asda"/>
+        </header>
+    )
+}
